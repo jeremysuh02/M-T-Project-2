936d64cf63d9fd63a7a737de0e565079c3a41639
TKADesigner <101240025+TKADesigner@users.noreply.github.com>
2022-04-19 21:13:14 -0500

    Initial Code
    Initial code made to demonstrate functionality.

--- m_t_Shrek/m_t_Shrek.ino
+++ m_t_Shrek/m_t_Shrek.ino
@@ -1,9 +1,57 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+unsigned int oldTime = 0;
+
+const int dragonServoPin = 2;
+const int dragonButtonPin = 12;
+
+const int fionaServoPin = 3;
+const int fionaButtonPin = 11;
+
+const int crowdServoPin = 4;
+const int crowdButtonPin1 = 10;
+const int crowdButtonPin2 = 9;
+
+
+Servo dragonServo;
+Servo fionaServo;
+Servo crowdServo;
+
+void setup() {
+  dragonServo.attach(dragonServoPin);
+  fionaServo.attach(fionaServoPin);
+  crowdServo.attach(crowdServoPin);
+  
+  pinMode(dragonButtonPin, INPUT);
+  pinMode(fionaButtonPin, INPUT);
+  pinMode(crowdButtonPin1, INPUT);
+  pinMode(crowdButtonPin2, INPUT);
+  
+  dragonServo.write(0);
+  fionaServo.write(0);
+  crowdServo.write(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (digitalRead(dragonButtonPin) == HIGH) {
+    dragonServo.write(90);
+    crowdServo.write(70);
+  }
 
+  if (digitalRead(fionaButtonPin) == HIGH) {
+    fionaServo.write(180);
+  }
+
+  if (digitalRead(crowdButtonPin1) == HIGH) {
+    crowdServo.write(35);
+  }
+  if (digitalRead(crowdButtonPin2) == HIGH) {
+    crowdServo.write(0);
+  }
+}
+unsigned long delta() {
+  unsigned long currentTime = millis();
+  unsigned long deltaTime = currentTime - oldTime;
+  oldTime = currentTime;
+  return deltaTime;
 }
